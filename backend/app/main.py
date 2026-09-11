eb48776b940c1c4e8bd82f8d83c2872186989f61
haleemajawad <haleemajawad2005@gmail.com>
2026-09-11 12:55:19 +0500

    issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://your-frontend.vercel.app",  # add your real Vercel URL once deployed
+        "https://saheli-ai-bay.vercel.app/",  # add your real Vercel URL once deployed
     ],
     allow_credentials=True,
     allow_methods=["*"],
